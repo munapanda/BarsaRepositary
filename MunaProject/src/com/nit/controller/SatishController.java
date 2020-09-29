5db999300e6b9587ea3e8a8ac4aca4ace143ee34
dev1 <dev@gmail.com>
2020-09-29 15:27:46 +0530

    Satish comiited

--- MunaProject/src/com/nit/controller/SatishController.java
+++ MunaProject/src/com/nit/controller/SatishController.java
@@ -4,6 +4,7 @@ public class SatishController {
 	public static void main(String[] args) {
 		System.out.println("Trichik");
 		System.out.println("Kuna is mendhu");
+		System.out.println("Satish bainkar");
 	}
 
 }
