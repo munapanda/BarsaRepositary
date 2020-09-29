714b58f57a848e8b3857989a6f7d179901d491a3
dev1 <dev@gmail.com>
2020-09-29 15:19:18 +0530

    Commited

---
+++ MunaProject/src/com/nit/controller/SatishController.java
@@ -0,0 +1,9 @@
+package com.nit.controller;
+
+public class SatishController {
+	public static void main(String[] args) {
+		System.out.println("Trichik");
+		System.out.println("Kuna is mendhu");
+	}
+
+}
